3bded2603e0b5cf10fffc4cbb52733743292e395
Vladimir Doroshenko <vovktt@gmail.com>
2022-01-22 23:48:23 +0500

    Added pydantic part 1

--- aiomodels/pydantic/model.py
+++ aiomodels/pydantic/model.py
@@ -1,40 +1,119 @@
 import typing as t
-import pydantic.main
 
-from aiomodels import base, core
+import pydantic
+import pydantic.main
+import pydantic.class_validators
 
 
 class ModelMetaclass(type):
+    def __new__(mcs, name, bases, attrs):
+        pydantic_fields = mcs.get_fields_for_pydantic(attrs)
+        pydantic_fields["__annotations__"] = mcs.get_annotations_for_pydantic(attrs)
+        mcs.create_model(name, bases, attrs, pydantic_fields)
 
-    def __new__(mcls, name, bases, attrs):
-        fields = {
-            key: (value, ...)
-            for key, value in (attrs.get("__annotations__") or {}).items()
-        }
-        model = pydantic.main.create_model(
-            name,
-            **fields
-        )
-        attrs['__annotations__'] = {}
-        attrs["_model"] = model
-
-        cls = super().__new__(mcls, name, bases, attrs)
-
-        print(cls)
-        print('\t', name)
-        print('\t', bases)
-        print('\t', attrs)
-        print('\t', model)
+        cls = super().__new__(mcs, name, bases, attrs)
 
         return cls
 
+    @classmethod
+    def get_annotations_for_pydantic(
+        mcs, attrs: t.Dict[str, t.Any]
+    ) -> t.Dict[str, t.Any]:
+        annotations = attrs.get("__annotations__", {})
+        result = {}
+        for key in list(annotations):
+            if not key.startswith("__"):
+                result[key] = annotations.pop(key)
+        return result
+
+    @classmethod
+    def get_fields_for_pydantic(mcs, attrs: t.Dict[str, t.Any]) -> t.Dict[str, t.Any]:
+        result = {}
+        for name in list(attrs):
+            value = attrs[name]
+
+            if hasattr(value, pydantic.class_validators.ROOT_VALIDATOR_CONFIG_KEY):
+                result[name] = attrs.pop(name)
+            elif hasattr(value, pydantic.class_validators.VALIDATOR_CONFIG_KEY):
+                result[name] = attrs.pop(name)
+            elif name.startswith("__") or callable(value):
+                continue
+            else:
+                result[name] = value
+
+        return result
+
+    @classmethod
+    def get_base_model(mcs, bases, attrs) -> t.Type[pydantic.main.BaseModel]:
+        attr = "__model__"
+        if attrs.get(attr):
+            return attrs[attr]
+
+        for cls in bases:
+            if base_model := getattr(cls, attr):
+                return base_model
+
+        raise RuntimeError("Attr __model__ not found")
+
+    @classmethod
+    def create_model(mcs, name: str, bases, attrs: t.Dict[str, t.Any], fields) -> None:
+        """
+        Create pydantic model
+
+        pydantic.main.create_model not working with annotations
+        """
+        model = type(
+            name,
+            (mcs.get_base_model(bases, attrs),),
+            fields,
+        )
+
+        attrs["__model__"] = model
+
+
 class Model(metaclass=ModelMetaclass):
+    __model__: t.ClassVar[t.Type[pydantic.main.BaseModel]] = pydantic.main.BaseModel
+    """Data storage schema"""
+    __instance__: pydantic.main.BaseModel = None
+    """Data storage"""
+
+    id: str = pydantic.Field(default=None, alias="_id")
+
+    class Config:
+        allow_population_by_field_name = False
 
     def __init__(self, *args, **kwargs) -> None:
-        self._instance: pydantic.main.BaseModel = self._model(*args, **kwargs)
+        self.__instance__ = self.__model__(*args, **kwargs)
 
-    def __str__(self):
-        return self._instance.__str__()
+    def __str__(self) -> str:
+        return str(self.__instance__)
 
-    def __repr__(self):
-        return self._instance.__repr__()
+    def __repr__(self) -> str:
+        return repr(self.__instance__)
+
+    def __getattr__(self, item: str) -> t.Any:
+        try:
+            return getattr(self.__instance__, item)
+        except AttributeError:
+            return super().__getattribute__(item)
+
+    def __setattr__(self, name: str, value: t.Any) -> None:
+        try:
+            object.__getattribute__(self, name)
+            super().__setattr__(name, value)
+        except AttributeError:
+            setattr(self.__instance__, name, value)
+
+    def __eq__(self, o: object) -> bool:
+        if isinstance(o, Model):
+            return self.__instance__ == o.__instance__
+        elif isinstance(o, pydantic.main.BaseModel):
+            return self.__instance__ == o
+        return super().__eq__(o)
+
+    def __ne__(self, o: object) -> bool:
+        if isinstance(o, Model):
+            return self.__instance__ != o.__instance__
+        elif isinstance(o, pydantic.main.BaseModel):
+            return self.__instance__ != o
+        return super().__ne__(o)
--- tests/pydantic/test_model.py
+++ tests/pydantic/test_model.py
@@ -1,25 +1,47 @@
 from unittest import TestCase
-from pydantic import validator, root_validator
+from pydantic import validator, root_validator, Field, BaseModel
 
 from aiomodels.pydantic.model import Model
 
 
 class TestModel(TestCase):
     def test_default(self):
-
-        class User(Model):
-            name: str
+        class Model1(Model):
+            a: str
+            b: str = None
+            c: str = Field(default_factory=lambda: "c")
+            d: str = Field("d")
 
             @root_validator(pre=True)
-            def a(self, values):
+            def root_val(cls, values):
                 return values
 
-            @validator("name")
-            def b(self, v):
+            @validator("a")
+            def a_val(cls, v):
                 return v
 
-        a = User(name='123')
-        print(1, a)
+            def method1(self):
+                pass
 
+        class Schema1(BaseModel):
+            id: str = Field(default=None, alias="_id")
+            a: str
+            b: str = None
+            c: str = Field(default_factory=lambda: "c")
+            d: str = Field("d")
 
+            @root_validator(pre=True)
+            def root_val(cls, values):
+                return values
 
+            @validator("a")
+            def a_val(cls, v):
+                return v
+
+        actual = Model1.__model__.schema()
+        expected = {
+            **Schema1.schema(),
+            'title': "Model1",
+        }
+
+        self.assertEqual(actual, expected)
